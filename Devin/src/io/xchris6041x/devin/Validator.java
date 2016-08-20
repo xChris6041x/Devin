bb3a660d680d51015a0f7eae3766ad01918b82aa
Christopher Bishop <christopherb6041@gmail.com>
2016-08-20 15:25:40 -0500

    Added MessageSender support ot Validator

--- Devin/src/io/xchris6041x/devin/Validator.java
+++ Devin/src/io/xchris6041x/devin/Validator.java
@@ -1,15 +1,19 @@
-package io.xchris6041x.devin.commands;
+package io.xchris6041x.devin;
 
 public final class Validator {
 
-	public static boolean isInteger(String str) {
+	public static boolean isInteger(String str, MessageSender msgSender) {
 		try {
 			Integer.parseInt(str);
 			return true;
 		}
 		catch (NumberFormatException e) {
+			if(msgSender != null) msgSender.error("Invalid number \"" + str + "\".");
 			return false;
 		}
+	}
+	public static boolean isInteger(String str) {
+		return isInteger(str);
 	}
 	
 }

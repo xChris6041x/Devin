299c3fd27721cf8f1f4bbb24857d330073cbf86c
Christopher Bishop <christopherb6041@gmail.com>
2016-08-20 15:30:11 -0500

    Added isDouble methods and documentations.

--- Devin/src/io/xchris6041x/devin/Validator.java
+++ Devin/src/io/xchris6041x/devin/Validator.java
@@ -2,9 +2,37 @@ package io.xchris6041x.devin;
 
 public final class Validator {
 
+	/**
+	 * @param str - The string to check.
+	 * @param msgSender - The message sender used to send a message on failed.
+	 * @return whether the string is an integer.
+	 */
 	public static boolean isInteger(String str, MessageSender msgSender) {
 		try {
 			Integer.parseInt(str);
+			return true;
+		}
+		catch (NumberFormatException e) {
+			if(msgSender != null) msgSender.error("Invalid integer \"" + str + "\".");
+			return false;
+		}
+	}
+	/**
+	 * @param str - The string to check.
+	 * @return whether the string is an integer.
+	 */
+	public static boolean isInteger(String str) {
+		return isInteger(str, null);
+	}
+	
+	/**
+	 * @param str - The string to check.
+	 * @param msgSender - The message sender used to send a message on failed.
+	 * @return whether the string is a double.
+	 */
+	public static boolean isDouble(String str, MessageSender msgSender) {
+		try {
+			Double.parseDouble(str);
 			return true;
 		}
 		catch (NumberFormatException e) {
@@ -12,8 +40,12 @@ public final class Validator {
 			return false;
 		}
 	}
-	public static boolean isInteger(String str) {
-		return isInteger(str, null);
+	/**
+	 * @param str - The string to check.
+	 * @return whether the string is a double.
+	 */
+	public static boolean isDouble(String str) {
+		return isDouble(str, null);
 	}
 	
 }

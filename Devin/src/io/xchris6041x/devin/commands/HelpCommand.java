e04c45c5bd659201900c1a16b60bf3d69d5cc3ff
Christopher Bishop <christopherb6041@gmail.com>
2016-08-21 01:51:32 -0500

    Added horizontal rule under the help header.

--- Devin/src/io/xchris6041x/devin/commands/HelpCommand.java
+++ Devin/src/io/xchris6041x/devin/commands/HelpCommand.java
@@ -50,6 +50,7 @@ public class HelpCommand implements CommandExecutor {
 		}
 		
 		msgSender.info(sender, cmdLabel.toUpperCase() + " Command Help (Page " + (page + 1) + ")");
+		msgSender.verbose(sender, "-----------------------------");
 		for(int i = 0; i < linesPerPage; i++) {
 			int index = page * linesPerPage + i;
 			if(help.size() <= index) break;

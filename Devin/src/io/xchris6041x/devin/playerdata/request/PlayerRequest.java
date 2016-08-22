b49d7a82ad7100bf7c3d99cf10e3eb171f55f2f9
Christopher Bishop <christopherb6041@gmail.com>
2016-08-22 15:30:05 -0500

    Added serialization so it can be stored in YAML.

--- Devin/src/io/xchris6041x/devin/playerdata/request/PlayerRequest.java
+++ Devin/src/io/xchris6041x/devin/playerdata/request/PlayerRequest.java
@@ -1,14 +1,17 @@
 package io.xchris6041x.devin.playerdata.request;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
 
 /**
  * @author Christopher Bishop
  */
-public abstract class PlayerRequest {
+public abstract class PlayerRequest implements ConfigurationSerializable {
 
 	private UUID sender;
 	private UUID receiver;
@@ -16,6 +19,10 @@ public abstract class PlayerRequest {
 	public PlayerRequest(Player sender, OfflinePlayer receiver) {
 		this.sender = sender.getUniqueId();
 		this.receiver = receiver.getUniqueId();
+	}
+	public PlayerRequest(Map<String, Object> map) {
+		sender = UUID.fromString((String) map.get("sender"));
+		receiver = UUID.fromString((String) map.get("receiver"));
 	}
 	
 	public abstract String getDescription();
@@ -33,6 +40,16 @@ public abstract class PlayerRequest {
 	 */
 	public UUID getReceiverUniqueId() {
 		return receiver;
+	}
+	
+	
+	@Override
+	public Map<String, Object> serialize() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("sender", sender.toString());
+		map.put("receiver", receiver.toString());
+		
+		return map;
 	}
 	
 }

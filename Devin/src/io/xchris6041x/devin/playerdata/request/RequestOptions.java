d6f6469b36d671c94d336206de0ab5666190a922
Christopher Bishop <christopherb6041@gmail.com>
2016-08-22 15:48:51 -0500

    Changed retention policy to RUNTIME.

--- Devin/src/io/xchris6041x/devin/playerdata/request/RequestOptions.java
+++ Devin/src/io/xchris6041x/devin/playerdata/request/RequestOptions.java
@@ -1,10 +1,16 @@
 package io.xchris6041x.devin.playerdata.request;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * @author Christopher Bishop
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RequestOptions {
 
+	public String name() default "Request";
+	
 	public String[] responses() default {"accept", "decline"};
 	
 }

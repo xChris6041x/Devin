fdec7f80928803a29d5ace4025f7a0db1ca6562a
Christopher Bishop <christopherb6041@gmail.com>
2016-08-22 14:50:53 -0500

    Change the name "from" and "to" to "sender" and "receiver".

--- Devin/src/io/xchris6041x/devin/playerdata/request/PlayerRequest.java
+++ Devin/src/io/xchris6041x/devin/playerdata/request/PlayerRequest.java
@@ -10,29 +10,29 @@ import org.bukkit.entity.Player;
  */
 public abstract class PlayerRequest {
 
-	private UUID from;
-	private UUID to;
+	private UUID sender;
+	private UUID receiver;
 	
-	public PlayerRequest(Player from, OfflinePlayer to) {
-		this.from = from.getUniqueId();
-		this.to = to.getUniqueId();
+	public PlayerRequest(Player sender, OfflinePlayer receiver) {
+		this.sender = sender.getUniqueId();
+		this.receiver = receiver.getUniqueId();
 	}
 	
 	public abstract String getDescription();
 	
 	
 	/**
-	 * @return the unique id of
+	 * @return the unique id of the player who sent the request.
 	 */
-	public UUID getFromUniqueId() {
-		return from;
+	public UUID getSenderUniqueId() {
+		return sender;
 	}
 	
 	/**
-	 * @return the uinque id of the player who was sent the request.
+	 * @return the uinque id of the player who received the request.
 	 */
-	public UUID getToUniqueId() {
-		return to;
+	public UUID getReceiverUniqueId() {
+		return receiver;
 	}
 	
 }

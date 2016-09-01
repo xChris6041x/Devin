4667f7a73bfc7613ca53e4391fc43548a8326675
Christopher Bishop <christopherb6041@gmail.com>
2016-09-01 09:41:42 -0500

    Made all the colors brighter.

--- Devin/src/io/xchris6041x/devin/AnsiColor.java
+++ Devin/src/io/xchris6041x/devin/AnsiColor.java
@@ -4,13 +4,13 @@ public final class AnsiColor {
 	
 	public static final String RESET = "\u001B[0m";
 	public static final String BLACK = "\u001B[30m";
-	public static final String RED = "\u001B[31m";
-	public static final String GREEN = "\u001B[32m";
-	public static final String YELLOW = "\u001B[33m";
-	public static final String BLUE = "\u001B[34m";
-	public static final String PURPLE = "\u001B[35m";
-	public static final String CYAN = "\u001B[36m";
-	public static final String WHITE = "\u001B[37m";
+	public static final String RED = "\u001B[1;31m";
+	public static final String GREEN = "\u001B[1;32m";
+	public static final String YELLOW = "\u001B[1;33m";
+	public static final String BLUE = "\u001B[1;34m";
+	public static final String PURPLE = "\u001B[1;35m";
+	public static final String CYAN = "\u001B[1;36m";
+	public static final String WHITE = "\u001B[1;37m";
 	
 	private AnsiColor(){
 	}

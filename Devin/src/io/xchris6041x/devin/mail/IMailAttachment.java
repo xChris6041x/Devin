6e92c7c039503bf8074f94f2b2038ade447b212b
Christopher Bishop <christopherb6041@gmail.com>
2016-09-01 12:09:58 -0500

    Added getUsage().

--- Devin/src/io/xchris6041x/devin/mail/IMailAttachment.java
+++ Devin/src/io/xchris6041x/devin/mail/IMailAttachment.java
@@ -3,7 +3,8 @@ package io.xchris6041x.devin.mail;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 
 public interface IMailAttachment extends ConfigurationSerializable {
-
+	
+	public String getUsage();
 	public boolean use(AttachableMail mail, String args[]);
 	
 }

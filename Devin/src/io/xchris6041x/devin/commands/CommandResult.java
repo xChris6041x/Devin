174cb2df8edbc207fee9676afbb146ceb1d2e679
Christopher Bishop <christopherb6041@gmail.com>
2016-09-01 09:34:20 -0500

    Built CommandResult

---
+++ Devin/src/io/xchris6041x/devin/commands/CommandResult.java
@@ -0,0 +1,43 @@
+package io.xchris6041x.devin.commands;
+
+/**
+ * Used to return the result of a command, this gives DEVIN more information than just true or false.
+ * @author Christopher Bishop
+ */
+public class CommandResult {
+	
+	public enum Status {
+		SUCCESS, FAILED, USAGE
+	}
+	
+	private Status status;
+	
+	private String message = null;
+	private boolean usePrefix = false;
+	
+	public CommandResult(Status status, String message, boolean usePrefix) {
+		this.status = status;
+		this.message = message;
+		this.usePrefix = usePrefix;
+	}
+	public CommandResult(Status status, String message) {
+		this(status, message, true);
+	}
+	public CommandResult(Status status) {
+		this(status, null);
+	}
+	public CommandResult() {
+		this(Status.SUCCESS);
+	}
+	
+	public Status getStatus() {
+		return status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public boolean usePrefix() {
+		return usePrefix;
+	}
+	
+}

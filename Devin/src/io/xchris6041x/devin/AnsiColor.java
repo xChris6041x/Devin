831dbe4559247800cb0716ccf397f635f44cfe3a
Christopher Bishop <christopherb6041@gmail.com>
2016-09-01 10:06:48 -0500

    Added all the dark colors.

--- Devin/src/io/xchris6041x/devin/AnsiColor.java
+++ Devin/src/io/xchris6041x/devin/AnsiColor.java
@@ -2,7 +2,10 @@ package io.xchris6041x.devin;
 
 public final class AnsiColor {
 	
+	// Reset
 	public static final String RESET = "\u001B[0m";
+	
+	// Colors
 	public static final String BLACK = "\u001B[30m";
 	public static final String RED = "\u001B[1;31m";
 	public static final String GREEN = "\u001B[1;32m";
@@ -11,6 +14,15 @@ public final class AnsiColor {
 	public static final String PURPLE = "\u001B[1;35m";
 	public static final String CYAN = "\u001B[1;36m";
 	public static final String WHITE = "\u001B[1;37m";
+	
+	// Dark Colors
+	public static final String DARK_RED = "\u001B[31m";
+	public static final String DARK_GREEN = "\u001B[32m";
+	public static final String DARK_YELLOW = "\u001B[33m";
+	public static final String DARK_BLUE = "\u001B[34m";
+	public static final String DARK_PURPLE = "\u001B[35m";
+	public static final String DARK_CYAN = "\u001B[36m";
+	public static final String GRAY = "\u001B[37m";
 	
 	private AnsiColor(){
 	}

678bb0f517bd1c05144b1140ae8a5b578c52387f
Christopher Bishop <christopherb6041@gmail.com>
2016-09-01 10:01:28 -0500

    Added short hand methods for building CommandResults.

--- Devin/src/io/xchris6041x/devin/commands/CommandResult.java
+++ Devin/src/io/xchris6041x/devin/commands/CommandResult.java
@@ -31,4 +31,79 @@ public class CommandResult {
 		return usePrefix;
 	}
 	
+	/**
+	 * Build a SUCCESS CommandResult.
+	 * @param message - The message.
+	 * @param usePrefix - Whether to use the message sender's prefix.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult success(String message, boolean usePrefix) {
+		return new CommandResult(Status.SUCCESS, message, usePrefix);
+	}
+	/**
+	 * Build a SUCCESS CommandResult where usePrefix = true.
+	 * @param message - The message.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult success(String message) {
+		return success(message, true);
+	}
+	/**
+	 * BUild a SUCCESS CommandResult with no message.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult success() {
+		return success(null);
+	}
+	
+	/**
+	 * Build a FAILED CommandResult.
+	 * @param message - The message.
+	 * @param usePrefix - Whether to use the message sender's prefix.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult failed(String message, boolean usePrefix) {
+		return new CommandResult(Status.FAILED, message, usePrefix);
+	}
+	/**
+	 * Build a FAILED CommandResult where usePrefix = true.
+	 * @param message - The message.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult failed(String message) {
+		return failed(message, true);
+	}
+	/**
+	 * BUild a FAILED CommandResult with no message.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult failed() {
+		return failed(null);
+	}
+	
+	/**
+	 * Build a USAGE CommandResult.
+	 * @param message - The message.
+	 * @param usePrefix - Whether to use the message sender's prefix.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult usage(String message, boolean usePrefix) {
+		return new CommandResult(Status.USAGE, message, usePrefix);
+	}
+	/**
+	 * Build a USAGE CommandResult where usePrefix = true.
+	 * @param message - The message.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult usage(String message) {
+		return usage(message, true);
+	}
+	/**
+	 * BUild a USAGE CommandResult with no message.
+	 * @return the built CommandResult.
+	 */
+	public static CommandResult usage() {
+		return usage(null);
+	}
+	
 }

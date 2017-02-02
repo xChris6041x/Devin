f65bfb73ac49a850fe5da9752f082d91b10b5872
Christopher Bishop <christopherb6041@gmail.com>
2017-02-02 13:04:05 -0600

    Removed the text field from Control.

--- src/io/xchris6041x/devin/gui/controls/Control.java
+++ src/io/xchris6041x/devin/gui/controls/Control.java
@@ -18,21 +18,7 @@ import io.xchris6041x.devin.gui.FrameHolder;
 public abstract class Control {
 
 	private int pos;
-	
-	private String text;
 	private IIconBuilder builder;
-	
-	/**
-	 * Create a control.
-	 * @param builder - What builds the icon.
-	 * @param position - The position in the inventory.
-	 * @param text - The text of the icon.
-	 */
-	public Control(IIconBuilder builder, int position, String text) {
-		this.builder = builder;
-		this.pos = position;
-		this.text = text;
-	}
 	
 	/**
 	 * Create a control.
@@ -40,17 +26,8 @@ public abstract class Control {
 	 * @param position - The position in the inventory.
 	 */
 	public Control(IIconBuilder builder, int position) {
-        this(builder, position, null);
-    }
-	
-	/**
-	 * Create a control.
-	 * @param icon - The icon that will be used.
-	 * @param position - The position in the inventory.
-	 * @param text - The text of the icon.
-	 */
-	public Control(ItemStack icon, int position, String text) {
-		this(new IconBuilder(icon), position, text);
+		this.builder = builder;
+		this.pos = position;
 	}
 	
 	/**
@@ -115,39 +92,12 @@ public abstract class Control {
     public void setY(int y) {
 	    setPosition(getX(), y);
     }
-
-	/**
-	 * @return the text is the display name of the ItemStack if the
-	 * ItemStack does not have a name. If the text is null, no change
-	 * will happen to the ItemStack display name.
-	 */
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
 	
 	/**
 	 * @return the builder of the icon.
 	 */
 	public IIconBuilder getIconBuilder() {
 		return builder;
-	}
-	
-	/**
-	 * @return the icon that will be used when rendering to the inventory.
-	 */
-	public ItemStack getIcon(Player player) {
-		ItemStack stack = builder.getIcon(player);
-		
-		ItemMeta meta = stack.getItemMeta();
-		if(!meta.hasDisplayName() && text != null) {
-			meta.setDisplayName(text);
-		}
-		stack.setItemMeta(meta);
-		
-		return stack;
 	}
 	
 	/**
@@ -162,7 +112,7 @@ public abstract class Control {
 	public void render(FrameHolder holder, Inventory inventory, int offset, int max) {
 		int pos = this.pos + offset;
 		if(pos >= 0 && pos < max) {
-			inventory.setItem(pos, getIcon(holder.getPlayer()));
+			inventory.setItem(pos, builder.getIcon(holder.getPlayer()));
 		}
 	}
 	
